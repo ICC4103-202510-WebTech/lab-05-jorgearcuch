7bd4e6fae8888a8d3e87d2c35e88121a7ea89b78
jorgearcuch <jjarcuch@miuandes.cl>
2025-04-09 16:12:08 -0400

    Lab-05

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |i|
+    User.create!(
+        email: "user{i+1}@gmail.com",
+        first_name: "FirstName #{i+1}",
+        last_name: "LastName #{i+1}",
+    )
+end
+
+10.times do |i|
+    Chat.create!(
+        sender_id: rand(1..10),
+        receiver_id: rand(1..10),
+    )
+end
+
+10.times do |i|
+    Message.create!(
+        chat_id: rand(1..10),
+        user_id: rand(1..10),
+        body: "Message body #{i+1}",
+    )
+end
